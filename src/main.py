3380e3e696604ef90329bb78148283a81710be9d
3erendira <87461596+3erendira@users.noreply.github.com>
2026-02-12 10:07:52 +0200

    Connect and update RPC in main function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("another rewrite? cool")
+from rpc import rpcClient
+import time
+
+def main():
+    RPC = rpcClient()
+    RPC.connectToRPC()
+    while True:
+        RPC.updateRPC(
+            state = "test_state",
+            details = "test_details",
+            name = "test_name"
+        )
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
