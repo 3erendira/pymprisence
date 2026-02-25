a7728112604f8431db60f7695bd9fc28c65d5126
3erendira <87461596+3erendira@users.noreply.github.com>
2026-02-25 02:41:25 +0200

    Update RPC with metadata and other information from MPRIS

--- src/main.py
+++ src/main.py
@@ -1,21 +1,30 @@
 from rpc import rpcClient
+from pypresence.types import ActivityType
+from logger import Logger
+from mpris import MPRIS
 import time
 import os
-from logger import Logger
 
 def main():
     os.makedirs(os.path.join(os.path.expanduser("~"), ".pymprisence/logs/"), exist_ok=True)
+    Logger().get()
+    conn = MPRIS()
+
+    metadata = conn.get_metadata()
+    position = conn.get_position()
 
     RPC = rpcClient()
     RPC.connectToRPC()
     while True:
         RPC.updateRPC(
-            state = "test_state",
-            details = "test_details",
-            name = "test_name"
+            details = metadata["xesam:title"][1],
+            state = ', '.join(metadata["xesam:artist"][1]),
+            name = "music",
+            activity_type = ActivityType.LISTENING,
+            length = metadata["mpris:length"][1] / 1000000,
+            position = position / 1000000
         )
         time.sleep(5)
 
 if __name__ == "__main__":
-    logger = Logger().get()
     main()
